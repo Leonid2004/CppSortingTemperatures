ab86737c52fdec3627af4405368e559e2289dfb2
Leonid2004 <leophilo7@gmail.com>
2021-01-18 09:31:04 +0100

    MainFile added

---
+++ Sorting temepatures/Sorting temepatures/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Sorting temepatures
+//
+//  Created by Leonid on 18.01.2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+vector <int> analyzeTemps (vector<int> t) {
+    
+}
+
+int main(int argc, const char * argv[]) {
+
+
+    
+    
+    
+    return 0;
+}
