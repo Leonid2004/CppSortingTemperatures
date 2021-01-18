0673e2fd26e745411c39624309594f543f24dbff
Leonid2004 <leophilo7@gmail.com>
2021-01-18 13:16:19 +0100

    Added some info in README

--- Sorting temepatures/Sorting temepatures/main.cpp
+++ Sorting temepatures/Sorting temepatures/main.cpp
@@ -6,16 +6,20 @@
 //
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 vector <int> analyzeTemps (vector<int> t) {
+    vector<int> T;
+    
+    return T;
     
 }
 
 int main(int argc, const char * argv[]) {
 
-
+    
     
     
     
