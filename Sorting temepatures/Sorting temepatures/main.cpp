a714e64b5d352e5aab99aaa2a640d35a52d32c3d
Leonid2004 <leophilo7@gmail.com>
2021-01-19 22:40:58 +0100

    Added main thingfs

--- Sorting temepatures/Sorting temepatures/main.cpp
+++ Sorting temepatures/Sorting temepatures/main.cpp
@@ -10,17 +10,38 @@
 
 using namespace std;
 
-vector <int> analyzeTemps (vector<int> t) {
-    vector<int> T;
+vector <int> analyzeTemps (vector<int> t,int vSize) {
+    vector<int> T;//final array which will consist of number of days
+    int value;
+    for (int i = 0; i < vSize; i++) {
+    	cout <<i+1<<"-temperature:"<<endl;
+    	cin>>value;
+    	t.push_back(value);
+	}
+    
+    
+    for (int i = 0; i < t.size();i++) {
+    	for (int j = i; j < t.size()-1;j++) {
+    		
+    		if (t[i] < t[j+1]) {
+    			T.push_back((j+1)-i);
+    			break;
+			}
+		}
+	}
     
     return T;
     
 }
 
-int main(int argc, const char * argv[]) {
+int main() {
 
+	vector <int> vV;
+   vector <int> result = analyzeTemps(vV,7);
     
-    
+    for (int i = 0; i < result.size();i++) {
+    	cout <<result[i]<<" ";
+	}
     
     
     return 0;
